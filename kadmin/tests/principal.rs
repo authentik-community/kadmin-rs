517a668ec26683978b947315ec7c47617e535505
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2024-12-06 16:43:29 +0100

    rust tests
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- kadmin/tests/principal.rs
+++ kadmin/tests/principal.rs
@@ -2,12 +2,15 @@
 #[cfg(feature = "client")]
 use anyhow::Result;
 #[cfg(feature = "client")]
-use kadmin::{KAdmin, KAdminImpl};
+use kadmin::{KAdmin, KAdminImpl, Principal, PrincipalAttributes};
 #[cfg(feature = "client")]
 use serial_test::serial;
 mod k5test;
 #[cfg(feature = "client")]
 use k5test::K5Test;
+mod util;
+#[cfg(feature = "client")]
+use util::random_string;
 
 macro_rules! gen_tests {
     () => {
@@ -59,7 +62,56 @@ macro_rules! gen_tests {
             let realm = K5Test::new()?;
             let kadmin = KAdmin::builder()
                 .with_password(&realm.admin_princ()?, &realm.password("admin")?)?;
-            dbg!(kadmin.get_principal(&realm.user_princ()?)?);
+            let princ = kadmin.get_principal(&realm.user_princ()?)?;
+            assert!(princ.is_some());
+            let princ = princ.unwrap();
+            assert_eq!(princ.name(), &realm.user_princ()?);
+            Ok(())
+        }
+
+        #[cfg(feature = "client")]
+        #[test]
+        #[serial]
+        fn create_principal() -> Result<()> {
+            let realm = K5Test::new()?;
+            let kadmin = KAdmin::builder()
+                .with_password(&realm.admin_princ()?, &realm.password("admin")?)?;
+            let princname = random_string(16);
+            let princ = Principal::builder(&princname).create(&kadmin)?;
+            assert_eq!(princ.name(), format!("{princname}@KRBTEST.COM"));
+            assert_eq!(princ.max_life(), Some(std::time::Duration::from_secs(86400)));
+            assert_eq!(princ.attributes(), PrincipalAttributes::from_bits_retain(0));
+            Ok(())
+        }
+
+        #[cfg(feature = "client")]
+        #[test]
+        #[serial]
+        fn delete_principal() -> Result<()> {
+            let realm = K5Test::new()?;
+            let kadmin = KAdmin::builder()
+                .with_password(&realm.admin_princ()?, &realm.password("admin")?)?;
+            let princname = random_string(16);
+            let princ = Principal::builder(&princname).create(&kadmin)?;
+            assert!(kadmin.principal_exists(&princname)?);
+            princ.delete(&kadmin)?;
+            assert!(!kadmin.principal_exists(&princname)?);
+            Ok(())
+        }
+
+        #[cfg(feature = "client")]
+        #[test]
+        #[serial]
+        fn modify_principal() -> Result<()> {
+            let realm = K5Test::new()?;
+            let kadmin = KAdmin::builder()
+                .with_password(&realm.admin_princ()?, &realm.password("admin")?)?;
+            let princname = random_string(16);
+            let princ = Principal::builder(&princname).create(&kadmin)?;
+            let princ = princ.modifier().attributes(PrincipalAttributes::RequiresPreAuth).modify(&kadmin)?;
+            assert_eq!(princ.attributes(), PrincipalAttributes::RequiresPreAuth);
+            let princ = kadmin.get_principal(&princname)?.unwrap();
+            assert_eq!(princ.attributes(), PrincipalAttributes::RequiresPreAuth);
             Ok(())
         }
 
@@ -77,6 +129,33 @@ macro_rules! gen_tests {
             princ.change_password(&kadmin, &realm.password("user")?, None, None)?;
             Ok(())
         }
+
+        #[cfg(feature = "client")]
+        #[test]
+        #[serial]
+        fn randkey() -> Result<()> {
+            let realm = K5Test::new()?;
+            let kadmin = KAdmin::builder()
+                .with_password(&realm.admin_princ()?, &realm.password("admin")?)?;
+            let princ = kadmin.get_principal(&realm.user_princ()?)?.unwrap();
+            princ.randkey(&kadmin, None, None)?;
+            assert!(realm.kinit(&realm.user_princ()?, "new_password").is_err());
+            // Restore password
+            princ.change_password(&kadmin, &realm.password("user")?, None, None)?;
+            Ok(())
+        }
+
+        #[cfg(feature = "client")]
+        #[test]
+        #[serial]
+        fn unlock() -> Result<()> {
+            let realm = K5Test::new()?;
+            let kadmin = KAdmin::builder()
+                .with_password(&realm.admin_princ()?, &realm.password("admin")?)?;
+            let princ = kadmin.get_principal(&realm.user_princ()?)?.unwrap();
+            assert!(princ.unlock(&kadmin).is_ok());
+            Ok(())
+        }
     };
 }
 
@@ -86,9 +165,11 @@ mod sync {
     #[cfg(feature = "client")]
     use anyhow::Result;
     #[cfg(feature = "client")]
-    use kadmin::{KAdminImpl, sync::KAdmin};
+    use kadmin::{KAdmin, KAdminImpl, Principal, PrincipalAttributes};
     #[cfg(feature = "client")]
     use serial_test::serial;
+    #[cfg(feature = "client")]
+    use crate::random_string;
 
     #[cfg(feature = "client")]
     use crate::K5Test;
