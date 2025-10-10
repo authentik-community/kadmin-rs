c5cc306a150584021b263ef7f97338227b7602c5
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-10-10 07:23:49 +0200

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- kadmin/build.rs
+++ kadmin/build.rs
@@ -2,6 +2,7 @@
 
 // build.rs
 use std::{
+    collections::HashSet,
     env,
     fs::read_to_string,
     path::{Path, PathBuf},
@@ -14,12 +15,40 @@ use strum::IntoEnumIterator;
 struct Kadm5Config {
     #[allow(dead_code)]
     variant: Kadm5Variant,
-    include_paths: Vec<PathBuf>,
+    include_paths: HashSet<PathBuf>,
+    library_paths: HashSet<PathBuf>,
+    libraries: HashSet<String>,
 }
 
 impl Kadm5Config {
     fn name(&self) -> &'static str {
         self.variant.name()
+    }
+
+    fn outputs(&self) {
+        println!(
+            "cargo::rustc-env=KADMIN_BUILD_{}_LIBRARY_PATHS={}",
+            self.name().to_uppercase(),
+            self.library_paths
+                .iter()
+                .map(|path| path.display().to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+        println!(
+            "cargo::rustc-env=KADMIN_BUILD_{}_LIBRARIES={}",
+            self.name().to_uppercase(),
+            self.libraries
+                .iter()
+                .cloned()
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+    }
+
+    fn extend(&mut self, other: &Self) {
+        self.library_paths.extend(other.library_paths.clone());
+        self.libraries.extend(other.libraries.clone());
     }
 }
 
@@ -67,6 +96,8 @@ impl Kadm5Variant {
 }
 
 fn generate_bindings(config: &Kadm5Config, out_path: &Path) {
+    config.outputs();
+
     let mut builder = bindgen::Builder::default()
         .header("src/wrapper.h")
         .allowlist_type("(_|)kadm5.*")
@@ -96,25 +127,6 @@ fn generate_bindings(config: &Kadm5Config, out_path: &Path) {
         .allowlist_var("ENCTYPE_.*")
         .allowlist_var("KRB5_KDB_SALTTYPE_.*")
         .allowlist_var("KRB5_TL_LAST_ADMIN_UNLOCK")
-        .allowlist_function("krb5_init_context")
-        .allowlist_function("krb5_free_context")
-        .allowlist_function("krb5_get_error_message")
-        .allowlist_function("krb5_free_error_message")
-        .allowlist_function("krb5_parse_name")
-        .allowlist_function("krb5_sname_to_principal")
-        .allowlist_function("krb5_free_principal")
-        .allowlist_function("krb5_unparse_name")
-        .allowlist_function("krb5_free_unparsed_name")
-        .allowlist_function("krb5_cc_get_principal")
-        .allowlist_function("krb5_cc_default")
-        .allowlist_function("krb5_cc_resolve")
-        .allowlist_function("krb5_cc_close")
-        .allowlist_function("krb5_get_default_realm")
-        .allowlist_function("krb5_free_default_realm")
-        .allowlist_function("krb5_string_to_enctype")
-        .allowlist_function("krb5_string_to_salttype")
-        .allowlist_function("krb5_enctype_to_string")
-        .allowlist_function("krb5_salttype_to_string")
         .clang_arg("-fparse-all-comments")
         .derive_default(true)
         .generate_cstr(true)
@@ -141,12 +153,21 @@ fn main() {
 
     if let Some(config) = find_kadm5(Kadm5Variant::Mit) {
         println!("cargo:rustc-cfg={}", config.name());
+        println!(
+            "cargo::warning=Found MIT Kerberos kadm5. Includes: {:?}. Links: {:?}. Libraries: {:?}",
+            config.include_paths, config.library_paths, config.libraries,
+        );
         generate_bindings(&config, &out_path);
         found_any = true;
     }
 
     if let Some(config) = find_kadm5(Kadm5Variant::Heimdal) {
         println!("cargo:rustc-cfg={}", config.name());
+        println!(
+            "cargo::warning=Found Heimdal Kerberos kadm5. Includes: {:?}. Links: {:?}. Libraries: \
+             {:?}",
+            config.include_paths, config.library_paths, config.libraries,
+        );
         generate_bindings(&config, &out_path);
         found_any = true;
     }
@@ -157,22 +178,31 @@ fn main() {
 }
 
 fn find_kadm5(variant: Kadm5Variant) -> Option<Kadm5Config> {
-    if let Some(config) = try_includes_override(variant) {
-        return Some(config);
+    let mut config = None;
+    if let Some(c) = try_overrides(variant) {
+        config = Some(c);
     }
 
-    if let Some(config) = try_krb5_config(variant) {
-        return Some(config);
+    if let Some(c) = try_krb5_config(variant) {
+        if let Some(config) = config.as_mut() {
+            config.extend(&c);
+        } else {
+            config = Some(c);
+        }
     }
 
-    if let Some(config) = try_pkg_config(variant) {
-        return Some(config);
+    if let Some(c) = try_pkg_config(variant) {
+        if let Some(config) = config.as_mut() {
+            config.extend(&c);
+        } else {
+            config = Some(c);
+        }
     }
 
-    None
+    config
 }
 
-fn try_includes_override(variant: Kadm5Variant) -> Option<Kadm5Config> {
+fn try_overrides(variant: Kadm5Variant) -> Option<Kadm5Config> {
     if let Ok(includes_override) = env::var(variant.includes_override_env_var()) {
         return Some(Kadm5Config {
             variant,
@@ -180,15 +210,26 @@ fn try_includes_override(variant: Kadm5Variant) -> Option<Kadm5Config> {
                 .split_whitespace()
                 .map(PathBuf::from)
                 .collect(),
+            library_paths: HashSet::new(),
+            libraries: HashSet::new(),
         });
     }
     None
 }
 
 fn try_krb5_config(variant: Kadm5Variant) -> Option<Kadm5Config> {
+    let mut config: Option<Kadm5Config> = None;
+    let libs = vec![Some("kadm-client"), Some("kadm-server"), None];
+
     if let Ok(bin_override) = env::var(variant.bin_override_env_var()) {
-        if let Some(config) = probe_krb5_config(&bin_override, variant) {
-            return Some(config);
+        for lib in libs.clone() {
+            if let Some(c) = probe_krb5_config(&bin_override, lib, variant) {
+                if let Some(config) = config.as_mut() {
+                    config.extend(&c);
+                } else {
+                    config = Some(c);
+                }
+            }
         }
     }
 
@@ -198,42 +239,45 @@ fn try_krb5_config(variant: Kadm5Variant) -> Option<Kadm5Config> {
     };
 
     for bin in bins {
-        if let Some(bin) = probe_krb5_config(bin, variant) {
-            return Some(bin);
+        for lib in libs.clone() {
+            if let Some(c) = probe_krb5_config(bin, lib, variant) {
+                if let Some(config) = config.as_mut() {
+                    config.extend(&c);
+                } else {
+                    config = Some(c);
+                }
+            }
         }
     }
 
-    None
+    config
 }
 
-fn probe_krb5_config(bin: &str, variant: Kadm5Variant) -> Option<Kadm5Config> {
-    let output = Command::new(bin)
-        .arg("--cflags")
-        .arg("kadm-client")
-        .output()
-        .or_else(|_| {
-            Command::new(bin)
-                .arg("--cflags")
-                .arg("kadm-server")
-                .output()
-        })
-        .or_else(|_| Command::new(bin).arg("--cflags").output())
-        .ok()?;
+fn probe_krb5_config(bin: &str, lib: Option<&str>, variant: Kadm5Variant) -> Option<Kadm5Config> {
+    let mut args = vec!["--cflags", "--libs"];
+    if let Some(lib) = lib {
+        args.push(lib);
+    }
+    let output = Command::new(bin).args(args).output().ok()?;
 
     if !output.status.success() {
         return None;
     }
 
-    let cflags = String::from_utf8_lossy(&output.stdout);
-    let include_paths: Vec<PathBuf> = cflags
+    let output = String::from_utf8_lossy(&output.stdout);
+    let include_paths: HashSet<PathBuf> = output
         .split_whitespace()
-        .filter_map(|flag| {
-            if flag.starts_with("-I") {
-                Some(PathBuf::from(&flag[2..]))
-            } else {
-                None
-            }
-        })
+        .filter_map(|flag| flag.strip_prefix("-I").map(|flag| PathBuf::from(&flag)))
+        .collect();
+    let library_paths: HashSet<PathBuf> = output
+        .split_whitespace()
+        .filter_map(|flag| flag.strip_prefix("-L").map(|flag| PathBuf::from(&flag)))
+        .collect();
+    let libraries: HashSet<String> = output
+        .split_whitespace()
+        .filter_map(|flag| flag.strip_prefix("-l"))
+        .filter(|lib| lib.contains("kadm5"))
+        .map(|lib| lib.to_owned())
         .collect();
 
     if include_paths.is_empty() {
@@ -246,7 +290,9 @@ fn probe_krb5_config(bin: &str, variant: Kadm5Variant) -> Option<Kadm5Config> {
         if header_path.exists() && verify_header_variant(&header_path, variant) {
             return Some(Kadm5Config {
                 variant,
-                include_paths: include_paths.clone(),
+                include_paths,
+                library_paths,
+                libraries,
             });
         }
     }
@@ -263,23 +309,29 @@ fn try_pkg_config(variant: Kadm5Variant) -> Option<Kadm5Config> {
             "kadm-server",
         ],
         Kadm5Variant::Heimdal => vec![
-            "heimdal-kadm-server",
             "heimdal-kadm-client",
+            "heimdal-kadm-server",
             "kadm-server",
             "kadm-client",
             "heimdal-kadm5",
         ],
     };
 
+    let mut config: Option<Kadm5Config> = None;
+
     for pkg_name in pkg_names {
         if let Ok(lib) = pkg_config::Config::new().probe(pkg_name) {
-            if let Some(config) = probe_from_pkg_config(lib, variant) {
-                return Some(config);
+            if let Some(c) = probe_from_pkg_config(lib, variant) {
+                if let Some(config) = config.as_mut() {
+                    config.extend(&c);
+                } else {
+                    config = Some(c);
+                }
             }
         }
     }
 
-    None
+    config
 }
 
 fn probe_from_pkg_config(lib: pkg_config::Library, variant: Kadm5Variant) -> Option<Kadm5Config> {
@@ -288,7 +340,13 @@ fn probe_from_pkg_config(lib: pkg_config::Library, variant: Kadm5Variant) -> Opt
         if header_path.exists() && verify_header_variant(&header_path, variant) {
             return Some(Kadm5Config {
                 variant,
-                include_paths: lib.include_paths.clone(),
+                include_paths: HashSet::from_iter(lib.include_paths.iter().cloned()),
+                library_paths: HashSet::from_iter(lib.link_paths.iter().cloned()),
+                libraries: lib
+                    .libs
+                    .into_iter()
+                    .filter(|lib| lib.contains("kadm5"))
+                    .collect(),
             });
         }
     }
--- kadmin/src/sys.rs
+++ kadmin/src/sys.rs
@@ -2,13 +2,40 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(missing_docs)]
+#![allow(clippy::exhaustive_structs)]
+#![allow(clippy::unseparated_literal_suffix)]
+#![allow(clippy::unreadable_literal)]
 
 #[cfg(mit)]
 pub mod mit {
+    pub fn library_paths() -> Vec<&'static str> {
+        env!("KADMIN_BUILD_MIT_LIBRARY_PATHS")
+            .split_whitespace()
+            .collect()
+    }
+
+    pub fn libraries() -> Vec<&'static str> {
+        env!("KADMIN_BUILD_MIT_LIBRARIES")
+            .split_whitespace()
+            .collect()
+    }
+
     include!(concat!(env!("OUT_DIR"), "/bindings_mit.rs"));
 }
 
 #[cfg(heimdal)]
 pub mod heimdal {
+    pub fn library_paths() -> Vec<&'static str> {
+        env!("KADMIN_BUILD_HEIMDAL_LIBRARY_PATHS")
+            .split_whitespace()
+            .collect()
+    }
+
+    pub fn libraries() -> Vec<&'static str> {
+        env!("KADMIN_BUILD_HEIMDAL_LIBRARIES")
+            .split_whitespace()
+            .collect()
+    }
+
     include!(concat!(env!("OUT_DIR"), "/bindings_heimdal.rs"));
 }
