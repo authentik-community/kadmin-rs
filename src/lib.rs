fcac4d99de0076f796d2a10b9ea8e7c91f858f80
Marc 'risson' Schmitt <marc.schmitt@risson.space>
2025-10-27 17:06:10 +0100

    wip
    Signed-off-by: Marc 'risson' Schmitt <marc.schmitt@risson.space>

--- src/lib.rs
+++ src/lib.rs
@@ -2,34 +2,62 @@
 //!
 //! This is a safe, idiomatic Rust interface to libkadm5.
 //!
-//! With the `client` feature:
+//! This library does not link against libkadm5, but instead loads it at runtime to be able to
+//! support multiple variants.
+//!
+//! It provides four features, all enabled by default, for the supported variants of libkadm5:
+//!
+//! - `mit_client`
+//! - `mit_server`
+//! - `heimdal_client`
+//! - `heimdal_server`
 //!
 //! ```no_run
-//! use kadmin::{KAdmin, KAdminImpl};
+//! use kadmin::{KAdm5Variant, KAdmin, KAdminImpl};
 //!
-//! # #[cfg(feature = "client")]
-//! # fn example() {
 //! let princ = "user/admin@EXAMPLE.ORG";
 //! let password = "vErYsEcUrE";
 //!
-//! let kadmin = KAdmin::builder().with_password(&princ, &password).unwrap();
+//! let kadmin = KAdmin::builder(KAdm5Variant::MitClient)
+//!     .with_password(&princ, &password)
+//!     .unwrap();
 //!
 //! dbg!("{}", kadmin.list_principals(None).unwrap());
-//! # }
 //! ```
-//!
-//! With the `local` feature:
 //!
 //! ```no_run
-//! use kadmin::{KAdmin, KAdminImpl};
+//! use kadmin::{KAdm5Variant, KAdmin, KAdminImpl};
 //!
-//! # #[cfg(feature = "local")]
-//! # fn example() {
-//! let kadmin = KAdmin::builder().with_local().unwrap();
+//! let kadmin = KAdmin::builder(KAdm5Variant::MitServer)
+//!     .with_local()
+//!     .unwrap();
 //!
 //! dbg!("{}", kadmin.list_principals(None).unwrap());
-//! # }
 //! ```
+//!
+//! # About compilation
+//!
+//! During compilation, all the enabled variants will be discovered and bindings will be generated
+//! from the discovered variants. If a variant cannot be discovered, it will not be available for
+//! use. The following environment variables are available to override that discovery process:
+//!
+//! To override the directories in which the `kadm5/admin.h` header will be searched for:
+//!
+//! - `KADMIN_MIT_CLIENT_INCLUDES`
+//! - `KADMIN_MIT_SERVER_INCLUDES`
+//! - `KADMIN_HEIMDAL_CLIENT_INCLUDES`
+//! - `KADMIN_HEIMDAL_SERVER_INCLUDES`
+//!
+//! To override the path to the `krb5-config` binary:
+//!
+//! - `KADM5_MIT_CLIENT_KRB5_CONFIG`
+//! - `KADM5_MIT_SERVER_KRB5_CONFIG`
+//! - `KADM5_HEIMDAL_CLIENT_KRB5_CONFIG`
+//! - `KADM5_HEIMDAL_SERVER_KRB5_CONFIG`
+//!
+//! Library paths will also be looked for, and forwarded so that at runtime, the library can be
+//! loaded. If it cannot find any, it will try to load a generic library from the system library
+//! paths. You can override the path the library is loaded from with [`sys::Library::from_path`].
 //!
 //! # About thread safety
 //!
